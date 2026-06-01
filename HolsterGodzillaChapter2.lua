90bbdcb575a9bd49b04229199d39915c0f880985
Zhu Xi Xi <1744785377@qq.com>
2026-06-01 19:59:17 +0800

    Update window title for Chapter 2

--- HolsterGodzillaChapter2.lua
+++ HolsterGodzillaChapter2.lua
@@ -3,7 +3,7 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "哥斯拉皮套Hub - 第二章",
+    Title = "哥斯拉皮套Hub - 第二夜",
     SubTitle = "by.小梦",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 360),
